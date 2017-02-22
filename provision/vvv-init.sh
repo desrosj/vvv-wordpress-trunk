b130467df26f134def65f3f54586c961e9dc9812
Jonathan Desrosiers <desrosj@gmail.com>
2017-02-21 20:33:11 -0500

    Add missing `$` signs for variables.

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -1,9 +1,9 @@
 # Provision WordPress Trunk
 
 # Make a database, if we don't already have one
-echo -e "\nCreating database 'wordpress_{VVV_SITE_NAME}' (if it's not already there)"
-mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS wordpress_{VVV_SITE_NAME}"
-mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON wordpress_{VVV_SITE_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
+echo -e "\nCreating database 'wordpress_${VVV_SITE_NAME}' (if it's not already there)"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS wordpress_${VVV_SITE_NAME}"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON wordpress_${VVV_SITE_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
 echo -e "\n DB operations done.\n\n"
 
 # Nginx Logs
@@ -17,9 +17,9 @@ if [[ ! -d "${VVV_PATH_TO_SITE}/public_html" ]]; then
   svn checkout "https://core.svn.wordpress.org/trunk/" "${VVV_PATH_TO_SITE}/public_html"
   cd ${VVV_PATH_TO_SITE}/public_html
   echo "Configuring WordPress trunk..."
-  noroot wp core config --dbname=wordpress_{VVV_SITE_NAME} --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
+  noroot wp core config --dbname=wordpress_${VVV_SITE_NAME} --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
 // Match any requests made via xip.io.
-if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^({VVV_SITE_NAME}.)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(.xip.io)\z/', \$_SERVER['HTTP_HOST'] ) ) {
+if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^(${VVV_SITE_NAME}.)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(.xip.io)\z/', \$_SERVER['HTTP_HOST'] ) ) {
     define( 'WP_HOME', 'http://' . \$_SERVER['HTTP_HOST'] );
     define( 'WP_SITEURL', 'http://' . \$_SERVER['HTTP_HOST'] );
 }
@@ -27,7 +27,7 @@ if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^({VVV_SITE_NAME}.)\d{1,3}\
 define( 'WP_DEBUG', true );
 PHP
   echo "Installing WordPress trunk..."
-  noroot wp core install --url={VVV_SITE_NAME}.dev --quiet --title="Local WordPress Trunk Dev" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
+  noroot wp core install --url=${VVV_SITE_NAME}.dev --quiet --title="Local WordPress Trunk Dev" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
 else
   echo "Updating WordPress trunk..."
   cd ${VVV_PATH_TO_SITE}/public_html
